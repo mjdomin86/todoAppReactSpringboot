377f188504b4fe3bbb47d455680927fd3abb3128
Marcos Domínguez <31966560+mjdomin86@users.noreply.github.com>
2023-07-07 23:06:21 +0200

    :white_check_mark: Update cypress test

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  pageLoadTimeout: 1000000,
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
